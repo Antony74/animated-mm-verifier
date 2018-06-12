2afd6753aba02893f8d7feea849b5fb9ae3aa971
Antony <akb@akb.me.uk>
2018-06-12 09:25:24 +0100

    Got this script working with streams.

--- prepare-mm.ts
+++ prepare-mm.ts
@@ -10,17 +10,35 @@ fs.readdir(__dirname + '/mm/', (dirErr, files) => {
 
     files.forEach((filename) => {
 
-        fs.readFile(__dirname + '/mm/' + filename, (err, dataIn) => {
+        const deflate = new Pako.Deflate({gzip: true, to: 'string'} as Pako.DeflateOptions);
+        const readStream = fs.createReadStream(__dirname + '/mm/' + filename, {encoding: 'binary'});
+        const writeStream = fs.createWriteStream(__dirname + '/public/' + filename + '.gz', {encoding: 'binary'});
 
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-
-            const dataOut = Pako.gzip(dataIn, {to: 'string'});
-            fs.writeFile(__dirname + '/public/' + filename + '.gz', dataOut, {encoding: 'binary'}, () => {});
+        readStream.on('data', (data) => {
+            deflate.push(data, false);
         });
 
+        readStream.on('error', (readError) => {
+            console.error(readError);
+            process.exit(1);
+        });
+
+        readStream.on('end', () => {
+            deflate.push('', true);
+        });
+
+        deflate.onData = (chunk: Pako.Data ) => {
+            writeStream.write(chunk);
+        };
+
+        deflate.onEnd = (status: number) => {
+            writeStream.end();
+        };
+
+        writeStream.on('error', (writeError) => {
+            console.error(writeError);
+            process.exit(1);
+        });
     });
 
 });
