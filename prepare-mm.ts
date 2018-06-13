15d8efabaf94eb455138a60e82c6c2b855bbfec7
Antony <akb@akb.me.uk>
2018-06-13 20:42:55 +0100

    Break each .gz file up into half-meg chunks.

--- prepare-mm.ts
+++ prepare-mm.ts
@@ -1,4 +1,6 @@
 
+const maxFileSize = 512 * 1024;
+
 import * as fs from 'fs';
 import * as Pako from 'pako';
 
@@ -10,9 +12,29 @@ fs.readdir(__dirname + '/mm/', (dirErr, files) => {
 
     files.forEach((filename) => {
 
-        const deflate = new Pako.Deflate({gzip: true, to: 'string'} as Pako.DeflateOptions);
+        let fileCount = 0;
+
+        function createWriteStream() {
+            ++fileCount;
+            let sFileCount: string = fileCount.toString();
+            while (sFileCount.length < 3) {
+                sFileCount = '0' + sFileCount;
+            }
+
+            const stream = fs.createWriteStream(__dirname + '/public/' + filename + '.gz.' + sFileCount, {encoding: 'binary'});
+
+            stream.on('error', (writeError) => {
+                console.error(writeError);
+                process.exit(1);
+            });
+
+            return stream;
+        }
+
+        const deflate = new Pako.Deflate({gzip: true, to: 'string', header: {name: filename}} as Pako.DeflateOptions);
         const readStream = fs.createReadStream(__dirname + '/mm/' + filename, {encoding: 'binary'});
-        const writeStream = fs.createWriteStream(__dirname + '/public/' + filename + '.gz', {encoding: 'binary'});
+        let writeStream = createWriteStream();
+        let bytesWritten = 0;
 
         readStream.on('data', (data) => {
             deflate.push(data, false);
@@ -24,21 +46,27 @@ fs.readdir(__dirname + '/mm/', (dirErr, files) => {
         });
 
         readStream.on('end', () => {
-            deflate.push('', true);
+            deflate.push(Buffer.from([]), true);
         });
 
         deflate.onData = (chunk: Pako.Data ) => {
+            if (bytesWritten + chunk.length > maxFileSize) {
+                const endChunk = chunk.slice(0, maxFileSize - bytesWritten);
+                writeStream.write(endChunk);
+                writeStream.end();
+                writeStream = createWriteStream();
+                bytesWritten = 0;
+                chunk = chunk.slice(endChunk.length);
+            }
+
             writeStream.write(chunk);
+            bytesWritten += chunk.length;
         };
 
         deflate.onEnd = (status: number) => {
             writeStream.end();
         };
 
-        writeStream.on('error', (writeError) => {
-            console.error(writeError);
-            process.exit(1);
-        });
     });
 
 });
