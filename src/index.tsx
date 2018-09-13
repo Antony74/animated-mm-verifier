dd49c9e81a1f5c342f82696c81ab65ccee05c554
Antony <akb@akb.me.uk>
2018-09-13 20:33:03 +0100

    Add resources and example code.  I'd like to know why redux devtools for Chrome is not working with this app.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,57 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
+import { Machine } from 'xstate';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createMiddleware, createReducer } from 'redux-xstate';
+
+const LOG_URL = '';
+
+const actionMap = {
+  log: (dispatch, state) => fetch(LOG_URL, {
+    method: 'POST',
+    body: JSON.stringify(state)
+  })
+};
+
+const stateChart = {
+  key: 'light',
+  initial: 'green',
+  states: {
+    green: {
+      on: {
+        TIMER: 'yellow'
+      }
+    },
+    yellow: {
+      on: {
+        TIMER: 'red'
+      }
+    },
+    red: {
+      on: {
+        TIMER: 'green'
+      },
+      onEntry: ['log']
+    }
+  }
+};
+
+const machine = Machine(stateChart);
+
+const store = createStore(
+  combineReducers({
+    machine: createReducer(machine.initialState)
+  }),
+  applyMiddleware(createMiddleware(machine, actionMap))
+);
+
+store.dispatch({type: 'TIMER'});
+
+console.log(store.getState());
+
+// state.machine.value === "yellow"
+
 function App() {
 
   return (
