06c78af8c642a496f7738b838696cdbbc0931f6e
Antony <akb@akb.me.uk>
2018-09-17 21:57:46 +0100

    Added more resources.

---
+++ prepare-mm.ts
@@ -0,0 +1,73 @@
+
+const maxFileSize = 512 * 1024;
+
+import * as fs from 'fs';
+import * as Pako from 'pako';
+
+fs.readdir(__dirname + '/mm/', (dirErr, files) => {
+
+    if (dirErr) {
+        console.error(dirErr);
+    }
+
+    files.forEach((filename) => {
+
+        let fileCount = 0;
+
+        function createWriteStream() {
+            ++fileCount;
+            let sFileCount: string = fileCount.toString();
+            while (sFileCount.length < 3) {
+                sFileCount = '0' + sFileCount;
+            }
+
+            const stream = fs.createWriteStream(__dirname + '/public/' + filename + '.gz.' + sFileCount, {encoding: 'binary'});
+
+            stream.on('error', (writeError) => {
+                console.error(writeError);
+                process.exit(1);
+            });
+
+            return stream;
+        }
+
+        const deflate = new Pako.Deflate({gzip: true, to: 'string', header: {name: filename}} as Pako.DeflateOptions);
+        const readStream = fs.createReadStream(__dirname + '/mm/' + filename, {encoding: 'binary'});
+        let writeStream = createWriteStream();
+        let bytesWritten = 0;
+
+        readStream.on('data', (data) => {
+            deflate.push(data, false);
+        });
+
+        readStream.on('error', (readError) => {
+            console.error(readError);
+            process.exit(1);
+        });
+
+        readStream.on('end', () => {
+            deflate.push(Buffer.from([]), true);
+        });
+
+        deflate.onData = (chunk: Pako.Data ) => {
+            if (bytesWritten + chunk.length > maxFileSize) {
+                const endChunk = chunk.slice(0, maxFileSize - bytesWritten);
+                writeStream.write(endChunk);
+                writeStream.end();
+                writeStream = createWriteStream();
+                bytesWritten = 0;
+                chunk = chunk.slice(endChunk.length);
+            }
+
+            writeStream.write(chunk);
+            bytesWritten += chunk.length;
+        };
+
+        deflate.onEnd = (status: number) => {
+            writeStream.end();
+        };
+
+    });
+
+});
+
