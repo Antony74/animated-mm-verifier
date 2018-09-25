6be4a7e902a3a1650cf0ed40e53ea9d2565967ad
Antony <akb@akb.me.uk>
2018-09-25 21:48:48 +0100

    Made a start on a chunk downloader (which ultimately should not live in index.tsx).

--- src/index.tsx
+++ src/index.tsx
@@ -2,38 +2,89 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 import { Machine } from 'xstate';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, Action } from 'redux';
 import { createMiddleware, createReducer } from 'redux-xstate';
+import { MachineConfig } from 'xstate/lib/types';
+import * as superagent from 'superagent';
 
-const LOG_URL = '';
+const url = 'public/set.mm';
 
-const actionMap = {
-  log: (dispatch, state) => fetch(LOG_URL, {
-    method: 'POST',
-    body: JSON.stringify(state)
-  })
+enum chunkDownloadActions {
+  incrementUrlChunk = 'INCREMENT_URL_CHUNK',
+  requestChunk = 'REQUEST_CHUNK'
+}
+
+class IncrementUrlChunkAction implements Action {
+  readonly type = chunkDownloadActions.incrementUrlChunk;
+}
+
+class RequestChunkAction implements Action {
+  readonly type = chunkDownloadActions.requestChunk;
+}
+
+interface ChunkDownloadState {
+  nextChunk: number;
+}
+
+const chunkDownloadInitialState: ChunkDownloadState = {
+  nextChunk: 1
 };
 
-const stateChart = {
-  key: 'light',
-  initial: 'green',
-  states: {
-    green: {
-      on: {
-        TIMER: 'yellow'
-      }
-    },
-    yellow: {
-      on: {
-        TIMER: 'red'
-      }
-    },
-    red: {
-      on: {
-        TIMER: 'green'
-      },
-      onEntry: ['log']
+interface State {
+  machine: any;
+  chunkDownload: ChunkDownloadState;
+}
+
+const effects = {
+  onRequestChunk: (dispatch, state: State) => {
+    let sFileCount: string = state.chunkDownload.nextChunk.toString();
+    while (sFileCount.length < 3) {
+        sFileCount = '0' + sFileCount;
     }
+
+    superagent
+      .get(url + '.gz.' + sFileCount)
+      .responseType('arraybuffer')
+      .then((response: superagent.Response) => {
+//            this.eState = State.ready;
+//          this.inflate.push(response.body, false);
+      }).catch((error: superagent.ResponseError) => {
+          if (error.status === 404 && this.fileIndex !== 1) {
+//                this.eState = State.eof;
+//              this.inflate.push(new ArrayBuffer(0), true);
+
+//              if (this.data.getLength() === 0) {
+//                  this.complete = true;
+//                  this.tokenSubject.complete();
+//              }
+
+          } else {
+//              this.tokenSubject.error(error);
+          }
+      });
+  }
+};
+
+function chunkDownloadReducer(state: ChunkDownloadState = chunkDownloadInitialState, action: Action): ChunkDownloadState {
+  switch (action.type) {
+    case chunkDownloadActions.incrementUrlChunk:
+      return {...state, nextChunk: ++state.nextChunk};
+  }
+
+  return state;
+}
+
+const stateChart: MachineConfig = {
+  initial: 'idle',
+  states: {
+    idle: {
+      on: {
+        REQUEST_CHUNK: 'requestChunk'
+      }
+    },
+    requestChunk: {
+      onEntry: ['onRequestChunk']
+    },
   }
 };
 
@@ -41,17 +92,16 @@ const machine = Machine(stateChart);
 
 const store = createStore(
   combineReducers({
-    machine: createReducer(machine.initialState)
+    machine: createReducer(machine.initialState),
+    chunkDownload: chunkDownloadReducer
   }),
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(createMiddleware(machine, actionMap)),
+  applyMiddleware(createMiddleware(machine, effects)),
 );
 
-store.dispatch({type: 'TIMER'});
-
-console.log(store.getState());
-
-// state.machine.value === "yellow"
+// store.dispatch(new RequestChunkAction());
+store.dispatch({type: chunkDownloadActions.requestChunk});
+store.dispatch({type: chunkDownloadActions.incrementUrlChunk});
 
 function App() {
 
